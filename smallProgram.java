e77cf716080630bcf773c64306ff3c66193c6be3
Gauche <fexploding@gmail.com>
2025-08-21 20:50:02 +0800

    smol

---
+++ smallProgram.java
@@ -0,0 +1,22 @@
+public class smallProgram {
+     
+    public static void main(String[] args) {
+        
+       
+        for (int row = 0; row < 3; row++) {
+          
+        
+                
+                System.out.println("*****");
+                System.out.println("****");
+                System.out.println("***");
+                System.out.println("**");
+
+            
+           
+        }
+
+        System.out.println();
+        
+    }
+}
